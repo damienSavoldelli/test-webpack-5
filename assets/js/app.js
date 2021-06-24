c14f907e4abbca50e0ced121df567a5a50f83ecb
damien <damien.savoldelli@gmail.com>
2021-06-25 00:35:01 +0200

    feat: append a component in body dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 let a = 'salue les gens';
 
 const [b, , c] = [1,2,3,4]
@@ -7,3 +9,15 @@ console.log('test')
 console.log(a)
 console.log(b)
 
+// test with lodash
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+window.onload = function() {
+  document.body.appendChild(component());
+}
